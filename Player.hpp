a1ac806e2d7c8f80d670c3b89aea5a125f9d7dfb
vryo1 <leonjos000.jl@gmail.com>
2025-04-23 19:11:03 -0700

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -11,14 +11,12 @@ public:
 class HumanPlayer1 : public Player {
 public:
     void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override {
-        if (game.checkShot(game.computerBoard, game.playerStats, row, col)) {
-            game.outputCurrentMove(row, col, true, "Player 1");
-            if (game.isWinner(game.playerStats)) {
-                winnerText.setString("Player 1 Wins!");
-                gameOver = true;
-            }
-        } else {
-            game.outputCurrentMove(row, col, false, "Player 1");
+        bool hit = game.checkShot(game.computerBoard, game.playerStats, row, col);
+        game.outputCurrentMove(row, col, hit, "Player 1");
+
+        if (hit && game.isWinner(game.playerStats)) {
+            winnerText.setString("Player 1 Wins!");
+            gameOver = true;
         }
     }
 };
@@ -26,14 +24,13 @@ public:
 class HumanPlayer2 : public Player {
 public:
     void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override {
-        if (game.checkShot(game.playerBoard, game.computerStats, row, col)) {
-            game.outputCurrentMove(row, col, true, "Player 2");
-            if (game.isWinner(game.computerStats)) {
-                winnerText.setString("Player 2 Wins!");
-                gameOver = true;
-            }
-        } else {
-            game.outputCurrentMove(row, col, false, "Player 2");
+        bool hit = game.checkShot(game.playerBoard, game.computerStats, row, col);
+        game.outputCurrentMove(row, col, hit, "Player 2");
+
+        if (hit && game.isWinner(game.computerStats)) {
+            winnerText.setString("Player 2 Wins!");
+            gameOver = true;
         }
     }
 };
+
