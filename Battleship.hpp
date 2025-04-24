cebca2636e0c28dc762cc4e4329bb73a9b6cc1d2
kwadafriyie <kwadafriyie@gmail.com>
2025-04-23 20:25:51 -0700

    Update Battleship.hpp

--- Battleship.hpp
+++ Battleship.hpp
@@ -1,23 +1,18 @@
-#pragma once
 #include "libraries.hpp"
+#include "shipfleet.hpp"
 
 constexpr int BOARD_SIZE = 10;
 
-struct Stats 
+struct Stats
 {
     int hits = 0;
     int misses = 0;
     int shots = 0;
 };
 
-struct Ship 
-{
-    std::string name;
-    int size;
-    char symbol;
-};
 
-class Battleship 
+
+class Battleship
 {
 public:
     Battleship();
@@ -26,18 +21,21 @@ public:
     std::vector<std::vector<char>> playerBoard;
     std::vector<std::vector<char>> computerBoard;
     Stats playerStats, computerStats;
-    std::vector<Ship> ships;
     std::ofstream logfile;
 
     void welcomeScreen();
     void initializeBoard(std::vector<std::vector<char>>& board);
     int selectWhoStarts();
     void displayBoard(const std::vector<std::vector<char>>& board, bool isPlayer);
-    void manuallyPlaceShips(std::vector<std::vector<char>>& board);
+    //void manuallyPlaceShips(std::vector<std::vector<char>>& board);
     void randomlyPlaceShips(std::vector<std::vector<char>>& board);
     bool checkShot(std::vector<std::vector<char>>& board, Stats& stats, int row, int col);
     bool isWinner(const Stats& stats) const;
     void outputCurrentMove(int row, int col, bool hit, const std::string& player);
     void outputStats(const std::string& playerName, const Stats& stats);
     void soundEffects(bool hit);
+    void checkFleetHit(std::vector<std::shared_ptr<Ship>>& fleet, int row, int col);
+private:
+    std::vector<std::shared_ptr<Ship>> playerFleet;
+    std::vector<std::shared_ptr<Ship>> computerFleet;
 };
