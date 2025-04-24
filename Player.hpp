c78a77525f09987551c36887208087d1be47c3a6
vryo1 <leonjos000.jl@gmail.com>
2025-04-23 19:17:40 -0700

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include "libraries.hpp"
-#include "Battleship.hpp"
+#include <SFML/Graphics.hpp>
+
+class Battleship; // Forward declaration
 
 class Player {
 public:
@@ -10,27 +11,10 @@ public:
 
 class HumanPlayer1 : public Player {
 public:
-    void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override {
-        bool hit = game.checkShot(game.computerBoard, game.playerStats, row, col);
-        game.outputCurrentMove(row, col, hit, "Player 1");
-
-        if (hit && game.isWinner(game.playerStats)) {
-            winnerText.setString("Player 1 Wins!");
-            gameOver = true;
-        }
-    }
+    void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override;
 };
 
 class HumanPlayer2 : public Player {
 public:
-    void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override {
-        bool hit = game.checkShot(game.playerBoard, game.computerStats, row, col);
-        game.outputCurrentMove(row, col, hit, "Player 2");
-
-        if (hit && game.isWinner(game.computerStats)) {
-            winnerText.setString("Player 2 Wins!");
-            gameOver = true;
-        }
-    }
+    void takeTurn(Battleship& game, int row, int col, bool& gameOver, sf::Text& winnerText) override;
 };
-
